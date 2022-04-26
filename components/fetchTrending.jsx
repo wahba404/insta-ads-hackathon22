019b4f01d2727cdbb2ce8d9085b5f9dea5c86269
wahba404 <ricky.wahba@algolia.com>
2022-04-26 15:59:00 -0700

    fixed request call

--- components/fetchTrending.jsx
+++ components/fetchTrending.jsx
@@ -12,12 +12,17 @@ export function fetchTrending () {
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ indexName: "prod_ECOM",
-            model: "trending-items",
-            threshold: 0,
-            maxRecommendations: 10 })
+            body: JSON.stringify({
+                "requests": [
+                    { "indexName": "prod_ECOM",
+                     "model": "trending-items",
+                     "threshold": 0,
+                     "maxRecommendations": 10 
+                    }
+                ]
+              })
         };
-        const response = fetch('https://u9uxvsi686-dsn.algolia.net/', requestOptions);
+        const response = fetch('https://u9uxvsi686-dsn.algolia.net/1/indexes/*/recommendations', requestOptions);
         const data = response.json();
         setHits(data);
     }, [])
